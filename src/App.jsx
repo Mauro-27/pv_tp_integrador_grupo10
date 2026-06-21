1666cf152b251b7e94cfe9cd96ff74ab42d4f118
Mauro-27 <mauroxcampos.27@gmail.com>
2026-06-21 17:01:01 -0300

    Arreglo Header y Footer Mauro Campos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
+import Footer from './components/Footer.jsx'; 
 import { Outlet } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Container, CssBaseline, Box } from '@mui/material';
 
 function App() {
   return(
-    <div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+ 
+      <CssBaseline /> 
+
       <Header/>
-      <Container maxWidth="lg" sx={{minHeight: '80vh', mt: 4, mb: 4}}>
+      
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4, flexGrow: 1 }}>
         <main>
           <Outlet /> 
         </main>
       </Container>
+      
       <Footer />
-    </div>
+      
+    </Box>
   );
 }
 
